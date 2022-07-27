e3f4ce8b6361f5423d66fdfa98cb50a830f985a8
marciocamello <marciocamello@oulook.com>
2022-07-27 18:49:21 +0100

    Add react route and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
-
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Button variant="primary" />
-            <Button variant="secondary" />
-            <Button variant="success" />
-            <Button variant="info" />
-            <Button variant="warning" />
-            <Button />
-
-            <GlobalStyle />
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
